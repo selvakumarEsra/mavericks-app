c2e97e957b80d7761e742d1102612502b4d2e079
Selvakumar Esra <selvakumar.esra@gmail.com>
2016-04-18 21:33:24 +0800

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web java -Dserver.port=$PORT $JAVA_OPTS -jar target/mavericks-app-0.0.1-SNAPSHOT.jar
---
+++ src/main/java/org/infiteloopers/maericks/RecordRepository.java
@@ -0,0 +1,8 @@
+package org.infiteloopers.maericks;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecordRepository extends JpaRepository<Record, Long> {
+}
